0bb05445cda05f1c01145908ac563be59150131d
project-nazmul <project.nazmul@gmail.com>
2024-08-15 12:49:05 +0600

    batch

---
+++ app/src/main/java/com/example/tabbar/adapter/MyAdapter.kt
@@ -0,0 +1,30 @@
+package com.example.tabbar.adapter
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.tabbar.fragments.Home
+import com.example.tabbar.fragments.Profile
+import com.example.tabbar.fragments.Setting
+
+class MyAdapter(fragmentManager: FragmentManager,lifecycle: Lifecycle):
+    FragmentStateAdapter(fragmentManager,lifecycle) {
+    override fun getItemCount(): Int {
+        return  3
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return  when(position){
+            0 -> {
+                Home()
+            }
+            1 -> {
+                Profile()
+            }
+            else -> {
+                Setting()
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/tabbar/fragments/Setting.kt
@@ -0,0 +1,27 @@
+package com.example.tabbar.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.example.tabbar.R
+
+class Setting : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_setting, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        var newText = view.findViewById<TextView>(R.id.name)
+        newText.text = "New Setting"
+    }
+}
